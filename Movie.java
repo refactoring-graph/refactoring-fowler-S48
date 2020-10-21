6438477bdf4e83baa565a66eb4b1d7a99ee9bc53
tomaz1502 <tomgm1502@gmail.com>
2020-10-21 11:46:38 -0300

    Commit 10

--- Movie.java
+++ Movie.java
@@ -43,4 +43,13 @@ public class Movie {
       }
       return thisAmount;
   }
+
+   public int getFrequentRenterPoints(int daysRented) {
+       // add bonus for a two day new release rental
+       if ((getPriceCode() == NEW_RELEASE) &&
+            daysRented > 1)
+           return 2;
+       else
+           return 1;
+   }
 }
